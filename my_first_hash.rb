bbcfcfd80d9bb42da7d043ddba83186496c7cfc5
LaNice Powell <lanicemp@aol.com>
2019-07-25 12:25:42 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
-def my_hash{
+def my_hash
    user_info = {"name"=> "LaNice Powell"}
-}
+
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
